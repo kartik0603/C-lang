13f290509abac2354568b2084db68d74df073958
kartik0603 <kartikhirapara800@gmail.com>
2023-10-18 20:19:48 +0530

    18/10/2023

--- C_Language/Chap 4.1/Question1.c
+++ C_Language/Chap 4.1/Question1.c
@@ -2,9 +2,14 @@
 int main()
 {
 
-    int a = 15;
-    printf(" Enter Any Number  15\t : %d\n", ++a);
-    printf(" Enter Any Number  15\t : %d\n", --a);
+    int a, result1, result2;
+    printf(" Enter Any Number  15:\t");
+    scanf("%d", &a);
+    result1 = ++a;
+    result2 = --a;
+
+    printf("Inctrement \t: %d\n", result1);
+    printf("Inctrement \t: %d\n", result2);
 
     return 0;
 }
