f5fe5396783532fdd2e258d54fb448889775b33e
kartik0603 <kartikhirapara800@gmail.com>
2023-10-18 20:13:21 +0530

    18/10/2023

---
+++ C_Language/Chap 4.1/Question1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+
+    int a = 15;
+    printf(" Enter Any Number  15\t : %d\n", ++a);
+    printf(" Enter Any Number  15\t : %d\n", --a);
+
+    return 0;
+}
---
+++ C_Language/Chap 4.1/Question2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+
+    int a, b, result, result1, result2, result3, result4;
+
+    printf("Enter a \t : ");
+    scanf("%d", &a);
+
+    printf("Enter b \t :");
+    scanf("%d", &b);
+
+    result = a + b;
+    result1 = b - a;
+    result2 = a * b;
+    result3 = b / a;
+    result4 = b % a;
+
+    printf("Addition of 10 and 20 Ans           \t: %d\n", result);
+    printf("Subtraction of 20 and 10 Ans        \t: %d\n", result1);
+    printf("Multiplication of 10 and 20 Ans     \t: %d\n", result2);
+    printf("Division of 20 and 10 Ans           \t: %d\n", result3);
+    printf("Modues of 20 and 10 Ans             \t: %d\n", result4);
+
+    return 0;
+}
