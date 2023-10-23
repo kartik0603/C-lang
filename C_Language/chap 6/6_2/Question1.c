b865f75454a109f6e8fdd1629cadaa36d2b99908
kartik0603 <kartikhirapara800@gmail.com>
2023-10-23 19:51:17 +0530

    23/10/2023

---
+++ C_Language/chap 6/6_2/Question1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+
+    int a, b, c;
+
+    printf(" Enter value of a \t:\n");
+    scanf("%d", &a);
+
+    printf(" Enter value of b \t:\n");
+    scanf("%d", &b);
+
+    printf(" Enter value of c \t:\n");
+    scanf("%d", &c);
+
+    if (a < b)
+    {
+        if (a < c)
+        {
+            printf("The Minimum Value is \t:%d", a);
+        }
+        else
+        {
+            printf("The Minimum Value is \t:%d", c);
+        }
+    }
+    else
+    {
+        if (b < c)
+        {
+            printf("The Minimum Value is \t:%d", b);
+        }
+        else
+        {
+            printf("The Minimum Value is \t:%d", c);
+        }
+    }
+    return 0;
+}
