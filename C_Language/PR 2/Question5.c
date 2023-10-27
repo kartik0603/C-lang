7eb66eab948e565242ae591b8aa2e8ad9219d3a6
kartik0603 <kartikhirapara800@gmail.com>
2023-10-27 20:01:45 +0530

    27/10/2023

--- C_Language/PR 2/Question5.c
+++ C_Language/PR 2/Question5.c
@@ -1,36 +1,4 @@
-// // #include <stdio.h>
-// // int main()
-// // {
-// //     int a;
-// //     int result1, result2, result3, result4;
 
-// //     printf(" Enter Your Elctricity UNIT \t : ");
-// //     scanf("%d", &a);
-
-// //     switch (a)
-// //     {
-// //     case 50:
-// //         result1 = 50 * a * 0.2 * 0.5;
-// //         printf(" Your Elctericity BILL is \t : %d", result1);
-// //         break;
-
-// //     case 100:
-// //         result2 = (100 - 50) * a * 0.2 * 0.75;
-// //         printf(" Your Elcetricity BILL is \t : %d \n", result2);
-// //         break;
-
-// //     case 200:
-// //         result3 = (200 - 100) * a * 0.20 * 1.20;
-// //         printf(" Your Electricity BILL is \t : %d \n", result3);
-// //         break;
-
-// //     case 250:
-// //         result4 = (250 - 200) * a * 0.2 * 1.50;
-// //         printf(" Your Electricity BILL is \t : %d \n", result4);
-// //         break;
-// //     }
-// //     return 0;
-// // }
 
 #include <stdio.h>
 int main()
