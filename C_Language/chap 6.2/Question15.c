aa16509d8dd917c6fa2159db33f19350270ec7ef
kartik0603 <kartikhirapara800@gmail.com>
2023-11-06 12:09:38 +0530

    06/11/2023

--- C_Language/chap 6.2/Question15.c
+++ C_Language/chap 6.2/Question15.c
@@ -1,1 +1,18 @@
 // Reverse  Any Digit Numbera Using For Loop
+#include <stdio.h>
+int main()
+{
+    int i, n, remainder, reverse = 0;
+    printf("Enter Any Number \t: ");
+    scanf("%d", &n);
+    // reverse = i;
+    for (i = n; n != 0; n = n / 10)
+    {
+
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+
+        printf("The number in reverse order is\t:%d\n", reverse);
+    }
+    return 0;
+}
