06cb2f4b64190bf8529df801b74bd747ca46848a
kartik0603 <kartikhirapara800@gmail.com>
2023-11-28 11:56:51 +0530

    28/11/2023

---
+++ C_Language/Looping with  Custom pattern 1/Question3.c
@@ -0,0 +1,31 @@
+/*
+Q.3 Write a Program to print the below pattern using nested for loop.
+1 2 3 4 5 5 4 3 2 1
+1 2 3 4     4 3 2 1
+1 2 3         3 2 1
+1 2             2 1
+1                 1
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j, s;
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        for (s = 5; s > i; s--)
+        {
+            printf("  ");
+        }
+        for (j = i; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
