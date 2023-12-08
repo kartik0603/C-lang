49ac9b4a1dc4308b42a6f893b84f9d4b759dbc9b
kartik0603 <kartikhirapara800@gmail.com>
2023-12-08 14:04:39 +0530

    08/12/2023

--- C_Language/chap 8.3/Question3.c
+++ C_Language/chap 8.3/Question3.c
@@ -1,1 +1,52 @@
 /* Q.3 Write a Program to find the sum of diagonal elements from a given 2D array. */
+
+#include <stdio.h>
+
+int main()
+{
+    int row, column;
+
+    printf("Enter ROW Size: ");
+    scanf("%d", &row);
+
+    printf("Enter COLUMN Size: ");
+    scanf("%d", &column);
+
+    int a[row][column];
+    int i, j, sum = 0;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("Enter Element at position [%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Matrix elements:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            if (i == j)
+            {
+                sum = sum + a[i][j];
+            }
+        }
+        printf("\n");
+    }
+
+    printf(" Sum of Diagonal elements is \t:%d ", sum);
+    return 0;
+}
--- C_Language/chap 8.3/Question4.c
+++ C_Language/chap 8.3/Question4.c
@@ -1,1 +1,81 @@
 /* Q.4 Write a Program to print and find the sum of all boundary elements from a given 5x5 2D array. */
+
+#include <stdio.h>
+
+int main()
+{
+    // int row, column;
+
+    // printf("Enter ROW Size: ");
+    // scanf("%d", &row);
+
+    // printf("Enter COLUMN Size: ");
+    // scanf("%d", &column);
+
+    int a[5][5];
+    int i, j, sum = 0;
+    float average;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            printf("Enter Element at position [%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Matrix elements:\n");
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            if (
+
+                ((i == 0) && (j >= 0 && j <= 4)) ||
+
+                ((i == 4) && (j >= 0 && j <= 4)) ||
+
+                ((j == 0) && (i >= 0 && i <= 4)) ||
+
+                ((j == 4) && (i >= 0 && i <= 4))
+
+            )
+
+            {
+                printf("%d ", a[i][j]);
+            }
+
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            if (
+
+                ((i == 0) && (j >= 0 && j <= 4)) ||
+
+                ((i == 4) && (j >= 0 && j <= 4)) ||
+
+                ((j == 0) && (i >= 0 && i <= 4)) ||
+
+                ((j == 4) && (i >= 0 && i <= 4))
+
+            )
+
+            {
+                sum += a[i][j];
+            }
+        }
+    }
+    printf("The sum of boundary elements of an Array \t:%d ", sum);
+    return 0;
+}
