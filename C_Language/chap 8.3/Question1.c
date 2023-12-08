8580d4726885b210e10eaf0a641ae49a25451354
kartik0603 <kartikhirapara800@gmail.com>
2023-12-08 10:35:55 +0530

    08/12/2023

--- C_Language/chap 8.3/Question1.c
+++ C_Language/chap 8.3/Question1.c
@@ -1,1 +1,52 @@
 /* Q.1 Write a Program to find the average of a given 2D array. */
+
+#include <stdio.h>
+
+int main()
+{
+    int row, column;
+
+    printf("Enter ROW Size: ");
+    scanf("%d", &row);
+
+    printf("Enter COLUMN Size: ");
+    scanf("%d", &column);
+
+    int a[row][column];
+    int i, j, sum = 0;
+    float average;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("Enter Element at position [%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Matrix elements:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            sum += a[i][j];
+        }
+    }
+
+    average = (float)sum / (row * column);
+
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
--- C_Language/chap 8.3/Question2.c
+++ C_Language/chap 8.3/Question2.c
@@ -1,1 +1,80 @@
 /* Q.2 Write a Program to perform the addition operation of two 2D arrays & store it in another array. Keep in mind that both array sizes must be the same. */
+
+#include <stdio.h>
+
+int main()
+{
+    int row, column;
+
+    printf("Enter ROW Size: ");
+    scanf("%d", &row);
+
+    printf("Enter COLUMN Size: ");
+    scanf("%d", &column);
+
+    int a[row][column], b[row][column], c[row][column];
+    int i, j, sum = 0;
+    float average;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("Enter Element of Array A at position [%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("Enter Element of Array B at position [%d][%d]: ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Matrix elements A:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Matrix elements B:\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+        printf("\n");
+    }
+
+    printf("Matrix elements c:\n");
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
