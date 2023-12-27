9ee66f82934f4aed5ff7e39d2502f19066ba8383
kartik0603 <kartikhirapara800@gmail.com>
2023-12-27 13:27:12 +0530

    27/12/2023

--- C_Language/chap 11.1/function.c
+++ C_Language/chap 11.1/function.c
@@ -1,38 +1,4 @@
-// #include <stdio.h>
 
-// int getInt()
-// {
-//     int n;
-//     scanf("%d", &n);
-//     return n;
-// }
-
-// int getArrayLength()
-// {
-//     int n;
-//     printf(" Enter Array Length\t:");
-//     scanf("%d", &n);
-//     return n;
-// }
-
-// void arryInput(int a[], int n)
-// {
-//     int i;
-//     for (i = 0; i < n; i++)
-//     {
-//         printf(" Enter Array Elements a[%d] = ", i);
-//         a[i] = getInt();
-//     }
-// }
-//  void arrySquare(int a[], int n)
-//  {
-//     int i, Square =1;
-//     for ( i = 0; i < n; i++)
-//     {
-//         Square =a[i]*a[i];
-//     }
-//     printf("Square of Array Elements %d ",Square);
-//  }
 
 #include <stdio.h>
 
